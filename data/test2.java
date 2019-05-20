c401409e3dbde87d6760e193a158f78d5d9ec29b
Omar Boukhris <omarboukhris@think-it.io>
2019-05-20 16:50:15 +0100

    added test source code for recursive globing

---
+++ data/test2.java
@@ -0,0 +1,19 @@
+class myclass {
+	class tmp { 
+		public int x ; 
+		class l {
+			public int m () {} ;
+		} ;
+	} ;
+	public int a ;
+	public int a2 ;
+	public int c2 ;
+	public int b () {} ;
+	public int b1 () {} ;
+} ;
+
+class myclass2 {
+	public int c ;
+	public int d () {} ;
+	public int e () {} ;
+} ;
