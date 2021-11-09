6f82e10470d72e42748a8fdee6af1cce40648549
omar <omarboukhris@gmail.com>
2021-11-09 22:42:01 +0100

    update grammar syntax

---
+++ PyCpp/data/test_srcs/itf2/test2.cxx
@@ -0,0 +1,25 @@
+
+module mymodule.test2 ;
+
+#include <iostream>
+#include <iostream.h>
+#include "parselib/local.hpp"
+#include <std/fstream>
+#include "parselib/local.h"
+
+class myclass {
+
+    /*!
+    doc goes here
+    */
+    constructor();
+
+    constructor(int a, const float b);
+
+    /*! \brief attr doxy doc */
+	public @py const long &a2 ;
+	private char *& c3;
+	public string &&blob;
+
+} ;
+
